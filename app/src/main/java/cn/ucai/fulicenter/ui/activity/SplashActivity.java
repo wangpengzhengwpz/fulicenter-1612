fb14b30abb1c8313287f1c983606f1b517b80f02
wangpengzheng <2061743907@qq.com>
2017-03-21 17:41:33 +0800

    闪屏界面获取数据库中的用户数据

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/SplashActivity.java
@@ -5,10 +5,17 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.FuLiCenterApplication;
+import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.dao.UserDao;
+import cn.ucai.fulicenter.model.utils.L;
+import cn.ucai.fulicenter.model.utils.SharePrefrenceUtils;
 import cn.ucai.fulicenter.view.MFGT;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final String TAG = SplashActivity.class.getSimpleName();
     int time = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,12 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                String username = SharePrefrenceUtils.getInstance().getUserName();
+                if (username != null) {
+                    User user = UserDao.getInstance(SplashActivity.this).getUserInfo(username);
+                    L.e(TAG, "user=" + user);
+                    FuLiCenterApplication.setCurrentUser(user);
+                }
                 MFGT.gotoMain(SplashActivity.this);
                 SplashActivity.this.finish();
             }
