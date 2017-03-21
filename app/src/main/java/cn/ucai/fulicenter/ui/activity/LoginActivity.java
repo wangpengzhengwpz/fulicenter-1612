f0cb4ff2687941957b453ce2c0dcf33eb8ea7743
wangpengzheng <2061743907@qq.com>
2017-03-21 08:49:08 +0800

    用户登录

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/LoginActivity.java
@@ -1,9 +1,11 @@
 package cn.ucai.fulicenter.ui.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
@@ -11,7 +13,16 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.FuLiCenterApplication;
 import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.model.bean.Result;
+import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.net.IUserModel;
+import cn.ucai.fulicenter.model.net.OnCompleteListener;
+import cn.ucai.fulicenter.model.net.UserModel;
+import cn.ucai.fulicenter.model.utils.CommonUtils;
+import cn.ucai.fulicenter.model.utils.MD5;
+import cn.ucai.fulicenter.model.utils.ResultUtils;
 import cn.ucai.fulicenter.view.MFGT;
 
 /**
@@ -22,12 +33,16 @@ public class LoginActivity extends AppCompatActivity {
     EditText tvUsername;
     @BindView(R.id.tv_password)
     EditText tvPassword;
+    String username, password;
+    IUserModel model;
+    ProgressDialog pd;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        model = new UserModel();
     }
 
     @OnClick({R.id.backClickArea, R.id.btn_login, R.id.btn_register})
@@ -45,10 +60,70 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    private void login() {
-
+    private void showDialog() {
+        pd = new ProgressDialog(LoginActivity.this);
+        pd.setMessage(getString(R.string.registering));
+        pd.show();
     }
 
+    private void login() {
+        if (checkInput()) {
+            showDialog();
+            model.login(LoginActivity.this, username, MD5.getMessageDigest(password),
+                    new OnCompleteListener<String>() {
+                        @Override
+                        public void onSuccess(String s) {
+                            Result result = ResultUtils.getResultFromJson(s, User.class);
+                            if (result != null) {
+                                if (result.isRetMsg()) {
+                                    User user = (User) result.getRetData();
+                                    if (user != null) {
+                                        loginSuccess(user);
+                                    }
+                                } else {
+                                    if (result.getRetCode() == I.MSG_LOGIN_UNKNOW_USER) {
+                                        CommonUtils.showShortToast(R.string.login_fail_unknow_user);
+                                    }
+                                    if (result.getRetCode() == I.MSG_LOGIN_ERROR_PASSWORD) {
+                                        CommonUtils.showShortToast(R.string.login_fail_error_password);
+                                    }
+                                }
+                            }
+                            pd.dismiss();
+                        }
+
+                        @Override
+                        public void onError(String error) {
+                            CommonUtils.showShortToast(R.string.login_fail);
+                            pd.dismiss();
+                        }
+                    });
+        }
+    }
+
+    private void loginSuccess(User user) {
+        FuLiCenterApplication.setCurrentUser(user);
+    }
+
+    private boolean checkInput() {
+        username = tvUsername.getText().toString().trim();
+        password = tvPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(username)) {
+            tvUsername.requestFocus();
+            tvUsername.setError(getString(R.string.user_name_connot_be_empty));
+            return false;
+        }
+        if (!username.matches("[a-zA-Z]\\w{5,15}")) {
+            tvUsername.requestFocus();
+            tvUsername.setError(getString(R.string.illegal_user_name));
+        }
+        if (TextUtils.isEmpty(password)) {
+            tvPassword.requestFocus();
+            tvPassword.setError(getString(R.string.password_connot_be_empty));
+            return false;
+        }
+        return true;
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
