d30a118418d3c450795f18a05255766784092114
wangpengzheng <2061743907@qq.com>
2017-03-27 19:41:51 +0800

    订单页面的非空验证

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/OrderActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/OrderActivity.java
@@ -3,9 +3,11 @@ package cn.ucai.fulicenter.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -55,6 +57,33 @@ public class OrderActivity extends AppCompatActivity {
 
     @OnClick(R.id.tv_order_buy)
     public void commitOrder() {
-
+        String receiveName = edOrderName.getText().toString();
+        if (TextUtils.isEmpty(receiveName)) {
+            edOrderName.setError("收货人姓名不能为空");
+            edOrderName.requestFocus();
+            return;
+        }
+        String mobile = edOrderPhone.getText().toString();
+        if (TextUtils.isEmpty(mobile)) {
+            edOrderPhone.setError("手机号码不能为空");
+            edOrderPhone.requestFocus();
+            return;
+        }
+        if (!mobile.matches("[\\d]{11}")) {
+            edOrderPhone.setError("手机号码格式错误");
+            edOrderPhone.requestFocus();
+            return;
+        }
+        String area = spinOrderProvince.getSelectedItem().toString();
+        if (TextUtils.isEmpty(area)) {
+            Toast.makeText(OrderActivity.this, "收货地区不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String address = edOrderStreet.getText().toString();
+        if (TextUtils.isEmpty(address)) {
+            edOrderStreet.setError("街道地址不能为空");
+           edOrderStreet.requestFocus();
+            return;
+        }
     }
 }
