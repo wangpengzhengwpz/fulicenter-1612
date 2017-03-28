2964b44ddc73eb566a3dbef86c2b26dc99ca4004
wangpengzheng <2061743907@qq.com>
2017-03-28 21:58:14 +0800

    添加三方分享ShareSDK

--- app/src/main/java/cn/ucai/fulicenter/application/FuLiCenterApplication.java
+++ app/src/main/java/cn/ucai/fulicenter/application/FuLiCenterApplication.java
@@ -3,6 +3,7 @@ package cn.ucai.fulicenter.application;
 import android.app.Application;
 import android.content.Context;
 
+import cn.sharesdk.framework.ShareSDK;
 import cn.ucai.fulicenter.model.bean.User;
 import cn.ucai.fulicenter.model.dao.UserDao;
 import cn.ucai.fulicenter.model.utils.L;
@@ -20,6 +21,7 @@ public class FuLiCenterApplication extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        ShareSDK.initSDK(this);
     }
 
     public static Context getInstance() {
