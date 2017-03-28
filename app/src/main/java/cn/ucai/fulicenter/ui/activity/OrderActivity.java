d4de28c1fe7d6774b310fca1411b2bec106a885f
wangpengzheng <2061743907@qq.com>
2017-03-28 13:04:05 +0800

    修改运行时错误，添加银联提供的jar包

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/OrderActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/OrderActivity.java
@@ -28,8 +28,6 @@ import cn.ucai.fulicenter.application.I;
 import cn.ucai.fulicenter.model.utils.L;
 import cn.ucai.fulicenter.ui.view.MFGT;
 
-import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
-
 /**
  * Created by Administrator on 2017/3/27.
  */
@@ -48,6 +46,7 @@ public class OrderActivity extends AppCompatActivity {
     @BindView(R.id.tv_order_price)
     TextView tvOrderPrice;
     int orderPrice = 0;
+    private static String URL = "http://218.244.151.190/demo/charge";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
